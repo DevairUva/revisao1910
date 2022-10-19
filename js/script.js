e66301b7eefa1b79efd2a4c10dce4181b3b33bf4
Devair Martins <devair.martins@gmail.com>
2022-10-19 20:57:56 -0300

    function

---
+++ js/script.js
@@ -0,0 +1,29 @@
+//capturando o primeiro p por id
+// const pId = document.getElementById("myId");
+
+// //capturando o h1 por querySelector (TAG)
+// const h1Tag = document.querySelector("h1");
+
+// //capturando o p por querySelector (ID)
+// const element = document.querySelector('#myID');
+
+// //capturando a classe por querySelector
+// const elememtClass = document.querySelector('.myClass')
+
+//inerText
+// const elementText = document.querySelector('h1');
+// elementText.innerHTML = 'Mentira, sou um innertext'
+// elementText.innerHTML += ' Vasco'a
+
+// const elementHtml = document.querySelector('h1');
+// elementHtml.innerHTML = '<em>Sou um h1 em itálico<span></em>'
+
+// const elementInput = document.querySelector('input');
+// elementInput.value = 'vem que vem'
+
+// const element = document.querySelector('body');
+// element.style.backgroundColor = 'brown';
+
+// function clickHere(){
+
+// }
